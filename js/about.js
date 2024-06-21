eb2ae258fdf5156c34eed32f58ce3ebb360eb0df
tinyfighterpilot <163216550+tinyfighterpilot@users.noreply.github.com>
2024-06-21 16:24:09 -0400

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,17 +1,1 @@
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("i1");
-  var b = document.getElementsByClassName("i2");
-  var c = document.getElementsByClassName("i3");
-  //need if or condition statement to change slideIndex for slideshow
-  if (n > x.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
+
