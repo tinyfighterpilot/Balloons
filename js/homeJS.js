e56ccb196a0552ca96e34bfcb9afc6cfa988d2b5
tinyfighterpilot <airmailtiee33@gmail.com>
2024-08-08 16:24:08 -0400

    addes pictures, changes to index, added practice php

---
+++ js/homeJS.js
@@ -0,0 +1,20 @@
+let index = 1;
+showDivs(index);
+function plusSlide(x){
+    showDivs(index += x);
+}
+function showDivs(x){
+    var slide = document.getElementsByClassName("slide");
+    if(x > slide.length){
+        index = 1;
+    }
+    if(x < 1){
+        index = 2;
+    }
+    var i;
+    for(i=0; i < slide.length; i++){
+        slide[i].style.display = "none";
+
+    }
+    slide[index - 1].style.display = "block";
+}
