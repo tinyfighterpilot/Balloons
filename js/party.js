e9ad9e475563a99c36bcef28e8611c7c45cf8f48
tinyfighterpilot <airmailtiee33@gmail.com>
2024-08-28 09:52:42 -0400

    party favors changes

--- js/party.js
+++ js/party.js
@@ -2,6 +2,7 @@
 let slideIndex = 1;
 let si = 1;
 let s3 = 1;
+let slideindex = 1;
 showDivs(slideIndex);
 showDiv(si);
 showDivv(s3);
@@ -13,6 +14,9 @@ function plusDiv(o){
 }
 function plusDivv(a){
   showDivv(s3 += a);
+}
+function plusDiv3(x){
+  showDiv3(slideindex += x);
 }
 function showDivs(n) {
   var i;
@@ -58,4 +62,20 @@ function showDivv(a){
 
   }
   p[s3-1].style.display = "block";
+}
+function showDiv3(x){
+  var i;
+  var w = document.getElementsByClassName("i4");
+
+  //need if or condition statement to change slideIndex for slideshow
+  if (x > w.length) {
+    slideindex = 1;
+  }
+  if (x < 1) {
+    slideindex = w.length;
+  }
+  for (i = 0; i < w.length; i++) {
+    w[i].style.display = "none";  
+  }
+  w[slideindex-1].style.display = "block"; 
 }
